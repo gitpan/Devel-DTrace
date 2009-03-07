12d35bee8868a8582577efa8dd7c20c892f9abfa
Andy Armstrong <andy@hexten.net>
2009-03-07 22:48:32 +0000

    Import of ANDYA/Devel-DTrace-0.11 from CPAN.
    gitpan-cpan-distribution: Devel-DTrace
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         ANDYA/Devel-DTrace-0.11.tar.gz
    gitpan-cpan-author:       ANDYA
    gitpan-cpan-maturity:     released

--- runops-loop.h
+++ runops-loop.h
@@ -12,6 +12,9 @@
 #undef PROBE_RETURN
 #undef RUNOPS_DTRACE
 #undef RUNOPS_SUB_EXIT
+#undef RUNOPS_SAVED_ENTERSUB
+#undef RUNOPS_ENTERSUB
+#undef RUNOPS_INSTALL
 
 #ifdef RUNOPS_FAKE
 
@@ -25,8 +28,11 @@
         printf( "RETURN(%s, %s, %d)\n", func, file, line ); \
     }
 
-#define RUNOPS_DTRACE   _runops_dtrace_fake
-#define RUNOPS_SUB_EXIT _runops_sub_exit_fake
+#define RUNOPS_DTRACE           _runops_dtrace_fake
+#define RUNOPS_SUB_EXIT         _runops_sub_exit_fake
+#define RUNOPS_SAVED_ENTERSUB   _runops_saved_entersub_fake
+#define RUNOPS_ENTERSUB         _runops_entersub_fake
+#define RUNOPS_INSTALL          _runops_install_fake
 
 #else
 
@@ -40,8 +46,11 @@ if ( PERLXS_SUB_ENTRY_ENABLED(  ) && func && file ) {       \
         PERLXS_SUB_RETURN( func, file, line );              \
     }
 
-#define RUNOPS_DTRACE   _runops_dtrace
-#define RUNOPS_SUB_EXIT _runops_sub_exit
+#define RUNOPS_DTRACE           _runops_dtrace
+#define RUNOPS_SUB_EXIT         _runops_sub_exit
+#define RUNOPS_SAVED_ENTERSUB   _runops_saved_entersub
+#define RUNOPS_ENTERSUB         _runops_entersub
+#define RUNOPS_INSTALL          _runops_install
 
 #endif
 
@@ -50,46 +59,60 @@ if ( PERLXS_SUB_ENTRY_ENABLED(  ) && func && file ) {       \
 
 STATIC void
 RUNOPS_SUB_EXIT( pTHX_ void *sub_name ) {
-    PROBE_RETURN( ( char * ) sub_name,
-                  CopFILE( PL_curcop ), CopLINE( PL_curcop ) );
+  PROBE_RETURN( ( char * ) sub_name,
+                CopFILE( PL_curcop ), CopLINE( PL_curcop ) );
 }
 
+OP *( *RUNOPS_SAVED_ENTERSUB ) ( pTHX );
+
+STATIC OP *
+RUNOPS_ENTERSUB( pTHX ) {
+  const OP *next_op = PL_op->op_next;
+  OP *got_op = RUNOPS_SAVED_ENTERSUB( aTHX );
+  if ( got_op != next_op ) {
+    char *sub_name = ( char * ) _sub_name( aTHX );
+    PROBE_ENTRY( sub_name, CopFILE( PL_curcop ), CopLINE( PL_curcop ) );
+    save_destructor_x( RUNOPS_SUB_EXIT, sub_name );
+  }
+  return got_op;
+}
+
+STATIC void
+RUNOPS_INSTALL( void ) {
+  if ( PL_ppaddr[OP_ENTERSUB] != RUNOPS_ENTERSUB ) {
+    RUNOPS_SAVED_ENTERSUB = PL_ppaddr[OP_ENTERSUB];
+    PL_ppaddr[OP_ENTERSUB] = RUNOPS_ENTERSUB;
+  }
+}
+
+/* TODO: We don't need to replace runops - we could instead just patch
+ * up the execution address for OP_ENTERSUB - then everything else would
+ * run at full speed. 
+ */
 STATIC int
 RUNOPS_DTRACE( pTHX ) {
-    const OP *last_op = NULL;
-    const OP *next_op = NULL;
-    I32 last_cxstack_ix = 0;
-    const char *last_func = NULL;
-    I32 eval_depth = 0;
+  const OP *last_op = NULL;
+  const OP *next_op = NULL;
 
-    /* TODO: Use save_destructor(_x) to install a hook that's called on
-     * scope exit so we match RETURNS with ENTRIES.
-     * TODO: Remove last_op, last_cxstack_ix, last_func and do the test
-     * /before/ calling the op.
-     */
+  while ( PL_op ) {
+    last_op = PL_op;
+    next_op = PL_op->op_next;
 
-    while ( PL_op ) {
-        last_op = PL_op;
-        next_op = PL_op->op_next;
-
-        if ( PL_op = CALL_FPTR( PL_op->op_ppaddr ) ( aTHX ), PL_op ) {
-            PERL_ASYNC_CHECK(  );
-        }
-
-        if ( IS_ENTERSUB( last_op ) ) {
-            /* If we just called XS we'll now be at the next op. If we
-             * called a Perl subroutine we'll be executing its first op
-             * instead. 
-             */
-            if ( PL_op != next_op ) {
-                char *sub_name = ( char * ) _sub_name( aTHX );
-                PROBE_ENTRY( sub_name, CopFILE( PL_curcop ),
-                             CopLINE( PL_curcop ) );
-                save_destructor_x( RUNOPS_SUB_EXIT, sub_name );
-            }
-        }
+    if ( PL_op = CALL_FPTR( PL_op->op_ppaddr ) ( aTHX ), PL_op ) {
+      PERL_ASYNC_CHECK(  );
     }
 
-    TAINT_NOT;
-    return 0;
+    /* If we just called XS we'll now be at the next op. If we
+     * called a Perl subroutine we'll be executing its first op
+     * instead. We're only interested in Perl subs.
+     */
+    if ( IS_ENTERSUB( last_op ) && PL_op != next_op ) {
+      char *sub_name = ( char * ) _sub_name( aTHX );
+      PROBE_ENTRY( sub_name, CopFILE( PL_curcop ), CopLINE( PL_curcop ) );
+      save_destructor_x( RUNOPS_SUB_EXIT, sub_name );
+    }
+  }
+
+  TAINT_NOT;
+  return 0;
 }
--- runops.c
+++ runops.c
@@ -5,34 +5,45 @@
 #include "plxsdtrace.h"
 #include "runops.h"
 
-// STATIC const char *_eval_ = "(eval)";
-
 STATIC CV *
-_curcv( pTHX_ I32 ix ) {
-    for ( ; ix > 0; ix-- ) {
-        const PERL_CONTEXT *const cx = &cxstack[ix];
-        if ( CxTYPE( cx ) == CXt_SUB || CxTYPE( cx ) == CXt_FORMAT )
-            return cx->blk_sub.cv;
-        else if ( CxTYPE( cx ) == CXt_EVAL && !CxTRYBLOCK( cx ) )
-            return PL_compcv;
-        else if ( ix == 0 && PL_curstackinfo->si_type == PERLSI_MAIN )
-            return PL_main_cv;
-    }
+_curcv( pTHX ) {
+  PERL_SI *st = PL_curstackinfo;
+  I32 ix = st->si_cxix;
 
-    return NULL;
+  /* It's unclear whether we really need all this given that we call
+   * _curcv on the first OP after subroutine entry - so presumably not
+   * much can have happened by then. 
+   */
+  for ( ;; ) {
+    const PERL_CONTEXT *const cx = &st->si_cxstack[ix];
+    if ( CxTYPE( cx ) == CXt_SUB || CxTYPE( cx ) == CXt_FORMAT )
+      return cx->blk_sub.cv;
+    else if ( CxTYPE( cx ) == CXt_EVAL && !CxTRYBLOCK( cx ) )
+      return PL_compcv;
+    else if ( ix == 0 ) {
+      if ( st->si_type == PERLSI_MAIN )
+        return PL_main_cv;
+      if ( st = st->si_prev, NULL == st )
+        break;
+      ix = st->si_cxix + 1;     /* add one because we always decrement */
+    }
+    ix--;
+  }
+
+  return NULL;
 }
 
 STATIC const char *
 _sub_name( pTHX ) {
-    const CV *const cv = _curcv( aTHX_ cxstack_ix );
-    if ( cv ) {
-        const GV *const gv = CvGV( cv );
-        if ( gv ) {
-            return GvENAME( gv );
-        }
+  const CV *const cv = _curcv( aTHX );
+  if ( cv ) {
+    const GV *const gv = CvGV( cv );
+    if ( gv ) {
+      return GvENAME( gv );
     }
+  }
 
-    return NULL;
+  return NULL;
 }
 
 #undef RUNOPS_FAKE
@@ -42,16 +53,25 @@ _sub_name( pTHX ) {
 
 STATIC bool
 _should_fake(  ) {
-    const char *fake = getenv( FAKE_ENV );
-    return fake && atoi( fake );
+  const char *fake = getenv( FAKE_ENV );
+  return fake && atoi( fake );
 }
 
 void
 runops_hook(  ) {
-    runops_proc_t runops =
-        _should_fake(  )? _runops_dtrace_fake : _runops_dtrace;
+#if 1
+  runops_proc_t runops =
+      _should_fake(  )? _runops_dtrace_fake : _runops_dtrace;
 
-    if ( PL_runops != runops ) {
-        PL_runops = runops;
-    }
+  if ( PL_runops != runops ) {
+    PL_runops = runops;
+  }
+#else
+  if ( _should_fake(  ) ) {
+    _runops_install_fake(  );
+  }
+  else {
+    _runops_install(  );
+  }
+#endif
 }
